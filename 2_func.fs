fa3f24e53454f90097bd7a4fe512e13da3a839e5
FedorSabeshkin <37352353+FedorSabeshkin@users.noreply.github.com>
2024-01-21 19:33:03 +0300

    Update 2_func.fs

--- 2_func.fs
+++ 2_func.fs
@@ -1,5 +1,5 @@
-let g n = n + 5 // int
+let g n = n + 5 // int -> int
 
 let gg  = fun n -> n + 5
 
-let h (x,y) = Math.Sqrt(x ** 2.0 + y ** 2.0) 
+let h (x,y) = System.Math.Sqrt(x ** 2.0 + y ** 2.0) 
