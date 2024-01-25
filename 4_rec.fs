21409911c04ef55ab837567727960acc3b848c47
FedorSabeshkin <37352353+FedorSabeshkin@users.noreply.github.com>
2024-01-25 21:34:12 +0300

    Create 4_rec.fs

---
+++ 4_rec.fs
@@ -0,0 +1,13 @@
+let rec fibo = function 
+ | 0  -> 0
+ | 1  -> 1
+ | n  -> fibo(n-2) + fibo(n-1)
+
+let rec sum = function 
+ | 0  -> 0
+ | 1  -> 1
+ | n  -> n + sum(n-1)
+
+let rec sum2 = function 
+ | (m,0) -> m 
+ | (m,n) -> m + sum2(m-1)
