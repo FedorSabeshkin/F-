dd53882c975474ef20d8306701a898be538bcc54
FedorSabeshkin <37352353+FedorSabeshkin@users.noreply.github.com>
2024-02-01 06:10:12 +0300

    Update 6_string.fs

--- 6_string.fs
+++ 6_string.fs
@@ -3,19 +3,20 @@ let rec pow = function
  | (n, 0) -> ""
  | (n, m) -> n + pow(n, m-1)
 
-// predicate
+ // predicate
 let rec isIthChar = function
- | ((s:string),(n:int),(c:char)) -> char(s.[n]) = c
+ | (s, i, c) when String.length(s) <= i -> false
+ | (s ,n, c) -> s.[n] = c
 
 // returns the number of occurrences of the character c in the string s, starting from position n 
 let rec occFromIthAccumulator = function
- | ((s:string), (n:int), (c:char), (amount:int)) when n < ((String.length s)) -> if (char(s.[n]) = c) 
+ | ((s), (n), (c), (amount)) when n < ((String.length s)) -> if (s.[n] = c) 
                                                                                  then
                                                                                     let nextAmount = amount + 1 
                                                                                     occFromIthAccumulator(s,n+1,c, nextAmount);
                                                                                  else
                                                                                     occFromIthAccumulator(s,n+1,c, amount);
- | ((s:string), (n:int), (c:char), (amount:int)) -> amount
+ | ((s), (n), (c), (amount)) -> amount
  
 let rec occFromIth = function
- | ((s:string),(n:int),(c:char)) -> occFromIthAccumulator (s,n,c,0)
+ | ((s),(n),(c)) -> occFromIthAccumulator (s,n,c,0)
