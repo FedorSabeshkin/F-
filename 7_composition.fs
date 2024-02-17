3c6f0900f8cb58cd8280f36fd220f332ed5181fa
FedorSabeshkin <37352353+FedorSabeshkin@users.noreply.github.com>
2024-02-17 15:33:21 +0300

    Update 7_composition.fs

--- 7_composition.fs
+++ 7_composition.fs
@@ -1,11 +1,12 @@
 let vat n x = x * (1.0 + float(n) / 100.0)
 
 
-let unvat n x = x / (1.0 + float(n) / 100.0)
+let unvat n x = x / ((float n)/100.0 + 1.0)
 
 
-let rec min f =
-    if f(0) = 0 then
-        0
-    else
-        1 + min(f << (+)1)
+let rec min f = 
+   if (f 0) = 0 then
+      0 
+   else
+      let composition = f << (+)1
+      1 + min composition
