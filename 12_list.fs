2d316107a0193519b467269ea4bc1f9cfc91a3cc
FedorSabeshkin <37352353+FedorSabeshkin@users.noreply.github.com>
2024-02-25 18:09:20 +0300

    Update 12_list.fs

--- 12_list.fs
+++ 12_list.fs
@@ -1,8 +1,16 @@
 // upto n = [1; 2; ...; n]
 let rec upto = 
 
+
+let rec dnto_with_accum = function
+  | (1, accummulator) -> 1 :: accummulator
+  | (n, accummulator) -> n :: accummulator
+
+
 // downto n = [n; n-1; n-2; ...; 1]
-let rec dnto = 
+let rec dnto = function
+  | 1 -> dnto_with_accum (1, []) 
+  | n -> dnto_with_accum (n, (dnto (n-1)))
 
 
 let rec evenn =
