f5d6750bfa744933c7c48f3c36aa938230480477
FedorSabeshkin <37352353+FedorSabeshkin@users.noreply.github.com>
2024-02-27 16:39:43 +0300

    Update 12_list.fs

--- 12_list.fs
+++ 12_list.fs
@@ -20,4 +20,24 @@ let rec dnto = function
   | n -> dnto_with_accum (n, (dnto (n-1)))
 
 
-let rec evenn =
+
+let rec calculate_n_even_with_accum = function
+  | (currentValue, currentRound, maxRound) when currentRound < maxRound  -> 
+    calculate_n_even_with_accum(currentValue+2, currentRound+1, maxRound)
+  | (currentValue, currentRound, maxRound) -> currentValue
+
+
+let rec calculate_n_even = function
+  | n -> calculate_n_even_with_accum(0, 1, n)
+    
+let rec evenn_with_accum = function
+  | (0, accummulator) -> 0 :: accummulator
+  | (n, accummulator) -> 
+    let updatedAccummulator = n :: accummulator
+    evenn_with_accum(n-2, updatedAccummulator)
+
+// evenn n = [0; 2; ...; n-2; n]
+let rec evenn = function
+  | n -> 
+    let maxValue = calculate_n_even n
+    evenn_with_accum (maxValue, [])
