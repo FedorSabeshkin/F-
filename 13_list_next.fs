ccf5b86d21d8b96c4f2002155ed59cf61ec21970
FedorSabeshkin <37352353+FedorSabeshkin@users.noreply.github.com>
2024-03-02 20:58:13 +0300

    Update 13_list_next.fs

--- 13_list_next.fs
+++ 13_list_next.fs
@@ -1,13 +1,31 @@
-let rec rmodd = 
+
+let rec rmodd = function
+  | [] -> []
+  | [x] -> []
+  | first :: (second :: tail) -> second :: (rmodd tail)
 
 
-let rec del_even = 
+let rec del_even = function
+  | [] -> []
+  | head :: tail when (head % 2 = 0) -> (del_even tail)
+  | head :: tail -> head :: (del_even tail)
 
 
-let rec multiplicity x xs =
+let rec multiplicity x xs = match xs with
+  | [] -> 0
+  | head :: tail when (head = x) -> 1 + (multiplicity x tail)
+  | _ :: tail -> multiplicity x tail
 
 
-let rec split = 
+let rec split = function
+  | [] -> ([], [])
+  | [x] -> ([x], [])
+  | first :: (second :: tail) ->
+        let (a, b) = split tail
+        (first :: a, second :: b)
 
 
-let rec zip (xs1,xs2) =
+let rec zip = function
+  | ([], []) -> []
+  | (h1 :: t1, h2 :: t2) -> (h1, h2) :: zip(t1, t2)
+  | _ -> failwith "different lengths"
