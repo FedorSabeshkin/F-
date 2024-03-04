cc5faa8a5e38618622c58f174ed2382fdfc1ac1a
FedorSabeshkin <37352353+FedorSabeshkin@users.noreply.github.com>
2024-03-04 22:16:16 +0300

    Create 14_practice.fs

---
+++ 14_practice.fs
@@ -0,0 +1,29 @@
+
+let rec sum (p, xs) = 
+
+
+let rec count (xs, n) = 
+
+
+let rec insert (xs, n) = 
+
+
+let rec intersect (xs1, xs2) = 
+
+
+let rec plus (xs1, xs2) = 
+
+
+let rec minus (xs1, xs2) = 
+
+
+let rec smallest = 
+
+
+let rec delete (n, xs) = 
+
+
+let rec sort = 
+
+
+let rec revrev =
