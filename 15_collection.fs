df1f02e12d003b2c8e73e5634f4010f8d5afc6b1
FedorSabeshkin <37352353+FedorSabeshkin@users.noreply.github.com>
2024-03-09 17:33:03 +0300

    Update 15_collection.fs

--- 15_collection.fs
+++ 15_collection.fs
@@ -1,1 +1,7 @@
-let list_filter f xs =
+let list_filter f xs = List.foldBack (fun head tail -> if (f head) then head :: tail else tail) xs []
+
+
+let sum (p, xs) = List.fold (fun acc item -> if (p item) then item + acc else acc) 0 xs
+
+
+let revrev = List.fold (fun acc item -> (List.rev item) :: acc) []
