44f5ace9f5a6ff47056bf70c3181ef3716547032
FedorSabeshkin <37352353+FedorSabeshkin@users.noreply.github.com>
2024-03-21 08:17:16 +0300

    Update 18_imperative.fs

--- 18_imperative.fs
+++ 18_imperative.fs
@@ -1,1 +1,20 @@
 
+let f n =
+    let mutable x = 1
+    let mutable i = 2
+    while i <= n do
+        x <- x * i
+        i <- i + 1
+    x
+
+
+let fibo n =
+    let mutable a = 1
+    let mutable b = 0
+    let mutable i = 0
+    while i < n do
+        let c = a + b
+        a <- b
+        b <- c
+        i <- i + 1
+    b
