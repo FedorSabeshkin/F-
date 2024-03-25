9f02d381a62b2bfd5b8a04926203b456ef636f70
FedorSabeshkin <37352353+FedorSabeshkin@users.noreply.github.com>
2024-03-25 07:05:36 +0300

    Update 19_lazy.fs

--- 19_lazy.fs
+++ 19_lazy.fs
@@ -1,1 +1,15 @@
 
+let rec fibo1 n n1 n2 =
+    if n = 0 then n2
+    else fibo1 (n - 1) (n1 + n2) n1
+
+let rec fibo2 n c =
+    if n = 0 then c 0
+    elif n = 1 then c 1
+    else fibo2 (n - 1) (fun f -> c(f + (fibo2 (n - 2) id)))
+
+let rec bigList n k =
+    let rec iter n acc =
+        if n = 0 then acc
+        else iter (n - 1) (1 :: acc)
+    iter n (k [])
