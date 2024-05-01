a467d1c09f9527ac8325ac8a2b0a518ac3bc56aa
FedorSabeshkin <37352353+FedorSabeshkin@users.noreply.github.com>
2024-05-01 17:55:01 +0300

    Update 20_lazy_seq.fs

--- 20_lazy_seq.fs
+++ 20_lazy_seq.fs
@@ -2,16 +2,15 @@ let even_seq = Seq.initInfinite (fun i -> (i + 1) * 2)
 
 let fac_seq_raw = Seq.initInfinite (
     fun i -> 
-        // определяем рекурсивную функцию
+        // define rec function
         let rec iter n acc =
             if n <= 1 then acc
-            // используем хвостовую рекурсию сразу вычисляя следующее значение аккамулятора перед 
-            // передачей его в функцию
+            // use tail recursion for optimization
             else iter (n - 1) (n * acc)
-        // вызываем рекурсивную функцию с дефолтным значением аккамулятора  = 1.    
+        // call rec funbc with default value for accamulator = 1.    
         iter i 1
 )
-
+// cache values for reach func
 let fac_seq = Seq.cache fac_seq_raw
 
 let seq_seq = Seq.initInfinite (fun n -> if n % 2 = 0 then n/2 else (-1) * (n + 1)/2)
