8c35ced325824fe3429ccc273af77ab0f90979df
FedorSabeshkin <37352353+FedorSabeshkin@users.noreply.github.com>
2024-05-01 17:45:38 +0300

    Update 20_lazy_seq.fs

--- 20_lazy_seq.fs
+++ 20_lazy_seq.fs
@@ -1,11 +1,13 @@
 let even_seq = Seq.initInfinite (fun i -> (i + 1) * 2)
 
-let fac_seq = Seq.initInfinite (
+let fac_seq_raw = Seq.initInfinite (
     fun i -> 
         let rec iter n acc =
             if n <= 1 then acc
             else iter (n - 1) (n * acc)
         iter i 1
 )
+
+let fac_seq = Seq.cache fac_seq_raw
 
 let seq_seq = Seq.initInfinite (fun n -> if n % 2 = 0 then n/2 else (-1) * (n + 1)/2)
