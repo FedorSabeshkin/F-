e8258178087e646d62855e3404aa49156b85c71d
FedorSabeshkin <37352353+FedorSabeshkin@users.noreply.github.com>
2024-05-02 18:58:03 +0300

    Update 20_lazy_seq.fs

--- 20_lazy_seq.fs
+++ 20_lazy_seq.fs
@@ -4,11 +4,11 @@ let fac_seq = Seq.initInfinite (
     fun i -> 
         // define rec function
         let rec iter n acc =
-            if n <= 1I then acc
+            if n <= 1 then acc
             // use tail recursion for optimization
-            else iter (n - 1I) (n * acc)
+            else iter (n - 1) (n * acc)
         // call rec function with default value for accumulator = 1.    
-        iter (bigint i) 1I
+        iter i 1
 )
 
 let seq_seq = Seq.initInfinite (fun n -> if n % 2 = 0 then n/2 else (-1) * (n + 1)/2)
