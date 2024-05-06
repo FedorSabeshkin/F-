beebc1cc847f0e4843ec5231854ce03bc8236278
FedorSabeshkin <37352353+FedorSabeshkin@users.noreply.github.com>
2024-05-06 18:45:25 +0300

    Update 21_seq_another_sintax.fs

--- 21_seq_another_sintax.fs
+++ 21_seq_another_sintax.fs
@@ -1,1 +1,23 @@
+// 0, -1, 1, -2, 2, -3, 3, ...
+let rec fun_seq_pos_neq i = seq {  
+  yield (0 - i)
+  yield i  
+  yield! fun_seq_pos_neq (i+1)
+}
+let seq_seq = seq {  
+  yield 0
+  yield! fun_seq_pos_neq 1
+}
 
+let rec fac_seq_fun current_factorial i = seq { 
+  let nextMultiplier  = i + 1
+  let next_factorial = current_factorial * nextMultiplier  
+  yield next_factorial 
+  yield! fac_seq_fun next_factorial nextMultiplier  
+}
+// factorial sequance
+let fac_seq = seq {   
+  yield 1 
+  yield 1 
+  yield! fac_seq_fun 1 1
+}
